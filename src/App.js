9632d6339b68c09a2481be64a6b7c35813b27e8b
YangJiyu98 <yang.jiyu.jy@gmail.com>
2020-05-28 21:15:05 +0800

    minor edits to import statements

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
 import './index.css';
-import { ReactComponent as BellIcon } from './icons/bell.svg';
-import { ReactComponent as MessengerIcon } from './icons/messenger.svg';
-import { ReactComponent as CaretIcon } from './icons/caret.svg';
-import { ReactComponent as PlusIcon } from './icons/plus.svg';
-import { ReactComponent as CogIcon } from './icons/cog.svg';
-import { ReactComponent as ChevronIcon } from './icons/chevron.svg';
-import { ReactComponent as ArrowIcon } from './icons/arrow.svg';
-import { ReactComponent as BoltIcon } from './icons/bolt.svg';
-
-import React, { useState, useEffect, useRef } from 'react';
-import { CSSTransition } from 'react-transition-group';
-
+import React from 'react';
 //Routing
-import {Route, Link} from 'react-router-dom';
-import Home from './Home';
-import Profile from './Profile';
 import NavMenu from './NavMenu';
-
 
 function App() {
   return (
