6a3732c487766ae130b317e04a9aaca7bd7d776e
YangJiyu98 <yang.jiyu.jy@gmail.com>
2020-05-29 15:10:42 +0800

    added backend

--- src/index.js
+++ src/index.js
@@ -2,12 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom';
+import {ApolloProvider} from '@apollo/react-hooks';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient } from 'apollo-client';
 
+// 2
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+})
+
+// 3
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
 
 ReactDOM.render(
-  <BrowserRouter>
+  <ApolloProvider client ={client}>
+    <BrowserRouter>
      <App />
-  </BrowserRouter>,
+    </BrowserRouter>
+  </ApolloProvider>,
   document.getElementById('root')
 );
+
