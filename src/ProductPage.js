401a01117dad0f1fdd0913a18c91814f40d6161e
YangJiyu98 <yang.jiyu.jy@gmail.com>
2020-05-29 16:39:44 +0800

    test

---
+++ src/ProductPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { gql } from 'apollo-boost';
+import { useQuery, useMutation } from '@apollo/react-hooks';
+
+function ProductPage() {
+    return (
+        <div>
+            <h1>Welcome HOME</h1>
+        </div>
+    );
+}
+
+export default ProductPage;
