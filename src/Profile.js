9c81425dbc7c828f0cb4d9398a1906922190fd89
Marcus Tan <marcustan26@hotmail.com>
2020-05-31 01:17:58 +0800

    minor changes to Profile.js

--- src/Profile.js
+++ src/Profile.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Chart from './Chart';
+import BrandOptions from './BrandOptions';
+import CategoryOptions from './CategoryOptions';
 
 function Profile() {
     return (
         <div> 
-            <h1>My profile</h1>
+            <h1 align='center'>My profile</h1>
+            <CategoryOptions align='center'/>
+            <BrandOptions/>
             <Chart />
         </div>
         
