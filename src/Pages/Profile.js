9a08440fc827fbd396f7e823bb6783b0a696fc36
YangJiyu98 <yang.jiyu.jy@gmail.com>
2020-06-29 12:23:17 +0800

    added data

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -54,17 +54,21 @@ function Profile() {
 
     const handleClick = (product, user) => {
         var dataset = [];
+        const val = ['#4D4D4D', '#999999', '#FFFFFF', '#F44E3B', '#FE9200', '#FCDC00', '#DBDF00', '#A4DD00', '#68CCCA', '#73D8FF', '#AEA1FF', '#FDA1FF', '#333333', '#808080', '#cccccc', '#D33115', '#E27300', '#FCC400', '#B0BC00', '#68BC00', '#16A5A5', '#009CE0', '#7B64FF', '#FA28FF', '#000000', '#666666', '#B3B3B3', '#9F0500', '#C45100', '#FB9E00', '#808900', '#194D33', '#0C797D', '#0062B1', '#653294', '#AB149E']
         for (var i = 0; i < product.length; i++) {
             if(product[i].userID === user.id && product[i].category === categoryValue) {
                 dataset.push({
                     label: product[i].name,
-                    data: product[i].priceArray.map(price => price/100)
+                    data: product[i].priceArray,
+                    fill: false,
+                    borderColor: val[i],
+                    backgroundColor: val[i],
                 });
             }
         }
         console.log(dataset);
         return(setChartData({
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            labels: ['January', 'February', 'March', 'April', 'May', 'June'],
             datasets: dataset
         }))
         
