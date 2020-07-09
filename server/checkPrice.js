94c2ee1829b95ab3a85e39ae38bf4623bdd26a0a
Marcus Tan <marcustan26@hotmail.com>
2020-07-09 15:25:01 +0800

    fixed checkPrice

--- server/checkPrice.js
+++ server/checkPrice.js
@@ -1,17 +1,29 @@
 const lazada = require('./Lazada');
-const q10 = require('./q10 ');
+const q10 = require('./q10');
 const amazon = require('./AmazonParser');
 
 async function checkPrice(url) {
-    if(url.contains('amazon')) {
-        return amazon.checkAmazonPrice(url);
-    } else if(url.contains('lazada')) {
-        return lazada.checkLazadaPrice(url);
-    } else if(url.contains('qoo10')) {
-        return q10.checkQ10Price(url);
+    console.log(url);
+    if(url) {
+        try {
+            if(url.includes('amazon')) {
+                return amazon.checkAmazonPrice(url);
+            } else if(url.includes('lazada')) {
+                return lazada.checkLazadaPrice(url);
+            } else if(url.includes('qoo10')) {
+                return q10.checkQ10Price(url);
+            } else {
+                return null;
+            }
+        } catch (err) {
+            console.log(err);
+        }
     } else {
+        console.log('invalid url');
         return null;
     }
 }
+
+// checkPrice("https://www.amazon.com/dp/B07G7PMVR9/ref=dp_cerb_2")
 
 exports.checkPrice = checkPrice;
