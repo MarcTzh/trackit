e01c962c34ae555ae1d13f7cb506dd6480f25d14
Marcus Tan <marcustan26@hotmail.com>
2020-07-16 20:44:14 +0800

    Updates

---
+++ precache-manifest.2f4e3cae7b063026ac1c58e0622dcb79.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eab12ba0b45d802c5c30088a90d82f3c",
+    "url": "/trackit/index.html"
+  },
+  {
+    "revision": "16fff6f5297e632622bb",
+    "url": "/trackit/static/css/2.1eb21bb0.chunk.css"
+  },
+  {
+    "revision": "d12c01b5478c1ea0dd41",
+    "url": "/trackit/static/css/main.a6ba5863.chunk.css"
+  },
+  {
+    "revision": "16fff6f5297e632622bb",
+    "url": "/trackit/static/js/2.c380d9b8.chunk.js"
+  },
+  {
+    "revision": "bb82171e5fe4c1c98683497f643b1eb6",
+    "url": "/trackit/static/js/2.c380d9b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d12c01b5478c1ea0dd41",
+    "url": "/trackit/static/js/main.01fac3b4.chunk.js"
+  },
+  {
+    "revision": "5a0f707ca10a61ce2b3f",
+    "url": "/trackit/static/js/runtime-main.e084d1da.js"
+  },
+  {
+    "revision": "c21465db09b51d48e8c3cb7d73e00785",
+    "url": "/trackit/static/media/Trackit_logo.c21465db.png"
+  }
+]);
